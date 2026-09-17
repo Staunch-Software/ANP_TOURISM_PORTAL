47b2cbbc9b4495927f0736647370fdd097765d78
krishnaveni <krishvinayak2313@gmail.com>
2026-09-17 10:28:26 +0530

    Delete frontend/src/Field.jsx

--- frontend/src/Field.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Field = ({ label, children, error, hint }) =>
-<div className="field"><label className="lbl">{label}</label>{children}
-    {hint && !error && <div className="xs mut mt8">{hint}</div>}{error && <div className="err-t">{error}</div>}</div>;
